aefa9f0b10d350c96e932b4bfd06da5f55fdd17e
Ba9A-tata <yogadzakkii@gmail.com>
2025-10-30 11:50:14 +0700

    membuat kolom nama lengkap

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -1,11 +1,18 @@
 package com.example.userinput
 
 import android.R.attr.value
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Outline
 import java.lang.reflect.Modifier
 
 @Composable
@@ -13,12 +20,28 @@ fun FormDataDiri(modifier: Modifier
 ){
     var textNama by remember { mutableStateOf("") }
     var textAlamat by remember {mutableStateOf("") }
-    var textJK by remember {mutableListOf(value"")}
+    var textJK by remember {mutableListOf("")}
 
-    var nama by remember {mutableListOf(value"")}
-    var alamat by remember {mutableListOf(value"")}
-    var jenis by remember {mutableListOf(value"")}
+    var nama by remember {mutableListOf("")}
+    var alamat by remember {mutableListOf("")}
+    var jenis by remember {mutableListOf("")}
 
+    val gender.List<String> = listOf("Laki-laki" ,"Perempuan")
 
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment =  Alignment.CenterHorizontally) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifer.width(250.dp),
+            label = { Text("Nama Lengkap") },
+            onValueChange = {
+                textNama it
+            }
+        )
+
+    }
 
 }
